205e010ac7fe34091f8d04ef205a371e90c0dd37
tlbmeister <tlbmeister0@gmail.com>
2023-04-14 14:34:54 -0400

    assessment spring

--- springbootassessment/src/main/java/cogent/controller/MovieController.java
+++ springbootassessment/src/main/java/cogent/controller/MovieController.java
@@ -1,13 +1,18 @@
 package cogent.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
 
 import cogent.entity.Movie;
 import cogent.service.MovieService;
@@ -26,5 +31,17 @@ public class MovieController {
 	@GetMapping("/movies")
 	public List<Movie> fetchMovieList(){
 		return service.fetchMovieList();
+	}
+	
+	@DeleteMapping("/movies")
+	public String delete(@RequestParam(value="id") Integer id) {
+		Optional<Movie> i = service.get(id);
+		service.delete(i.get());
+		return "Item id "+id+" deleted successfully";
+	}
+	
+	@PutMapping("/items")
+	public Movie edit(@RequestBody Movie movie) {
+		return service.update(movie);
 	}
 }
